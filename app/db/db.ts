4ba7ea1fd1d6386e3eade70f19813cb994cec005
Hampton Hoover <hamphoover@gmail.com>
2024-04-23 14:05:36 -0500

    test branch commit

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from '@vercel/postgres';
 import * as UsersSchema from "./users/users-schema"
+import * as DataSetsSchema from "./datasets/datasets-schema"
 
-export const db = drizzle(sql, { schema: { ...UsersSchema } });
+export const db = drizzle(sql, { schema: { ...UsersSchema, ...DataSetsSchema } });
 
